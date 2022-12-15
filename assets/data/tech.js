ab2a065f0dafaecae6492376e38f5ef0bd7c0cb9
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-15 18:19:11 -0300

    abstraindo código

---
+++ assets/data/tech.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    name: 'Figma',
+    progress: 70,
+    icon: 'https://cdn.sanity.io/images/599r6htc/localized/46a76c802176eb17b04e12108de7e7e0f3736dc6-1024x1024.png?w=670&h=670&q=75&fit=max&auto=format',
+  },
+  {
+    name: 'Bootstrap',
+    progress: 70,
+    icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968672.png',
+  },
+  {
+    name: 'HTML',
+    progress: 70,
+    icon: 'https://cdn-icons-png.flaticon.com/512/732/732212.png',
+  },
+  {
+    name: 'JS',
+    progress: 60,
+    icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968292.png',
+  },
+  {
+    name: 'CSS',
+    progress: 55,
+    icon: 'https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png',
+  },
+  {
+    name: 'Github',
+    progress: 50,
+    icon: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+  },
+  {
+    name: 'Git',
+    progress: 40,
+    icon: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
+  },
+  {
+    name: 'React',
+    progress: 20,
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
+  },
+  {
+    name: 'MySQL',
+    progress: 20,
+    icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968313.png',
+  },
+  {
+    name: 'Node',
+    progress: 20,
+    icon: 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png',
+  },
+];
+
+export default data;
